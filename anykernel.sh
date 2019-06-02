0bed99f4ba9c76495cac29ad56be5b3c63c1bd6f
mortezadadgar <mortezadadgar97@gmail.com>
2019-06-02 15:57:05 +0430

    Add bullhead ramdisk
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,21 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Shadow Kernel for the Nexus 5x by @mr.ace
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=bullhead
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -37,25 +37,17 @@ chown -R root:root $ramdisk/*;
 ## AnyKernel install
 dump_boot;
 
+
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# fstab.bullhead
+patch_fstab fstab.bullhead none swap flags "zramsize=533413200" "zramsize=1066826400";
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# init.exec.rc
+insert_line init.bullhead.rc "init.exec.rc" after "import init.bullhead.ramdump.rc" "import init.exec.rc";
 
 # end ramdisk changes
+
 
 write_boot;
 ## end install
---
+++ ramdisk/init.shadow.sh
@@ -0,0 +1,107 @@
+#!/system/bin/sh
+
+################################################################################
+# helper functions to allow Android init like script
+
+function write() {
+    echo -n $2 > $1
+}
+
+function get-set-forall() {
+    for f in $1 ; do
+        cat $f
+        write $f $2
+    done
+}
+
+################################################################################
+
+
+# according to Qcom this legacy value improves first launch latencies
+setprop dalvik.vm.heapminfree 2m
+
+# disable thermal bcl hotplug to switch governor
+write /sys/module/msm_thermal/core_control/enabled 0
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode disable
+bcl_hotplug_mask=`get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_mask 0`
+bcl_hotplug_soc_mask=`get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask 0`
+
+# some files in /sys/devices/system/cpu are created after the restorecon of
+# /sys/. These files receive the default label "sysfs".
+restorecon -R /sys/devices/system/cpu
+
+# ensure at most one A57 is online when thermal hotplug is disabled
+write /sys/devices/system/cpu/cpu4/online 1
+write /sys/devices/system/cpu/cpu5/online 0
+
+# files in /sys/devices/system/cpu4 are created after enabling cpu4.
+# These files receive the default label "sysfs".
+# Restorecon again to give new files the correct label.
+restorecon -R /sys/devices/system/cpu
+
+# Little cluster
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 120
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "85 600000:45 787200:50 960000:60 1248000:80"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 60000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack 30000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 0
+
+# big cluster
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay "20000 960000:40000 1248000:20000"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 90
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 960000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "90 1248000:95"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 30000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 20000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_slack 30000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 0
+
+# plugin remaining A57s
+write /sys/devices/system/cpu/cpu5/online 1
+
+# cpu-boost
+write /sys/module/cpu_boost/parameters/input_boost_freq "0:1248000 4:768000"
+write /sys/module/cpu_boost/parameters/input_boost_ms 200
+
+# devfreq
+get-set-forall /sys/class/devfreq/qcom,cpubw*/governor bw_hwmon
+get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/io_percent 20
+get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/guard_band_mbps 30
+get-set-forall /sys/class/devfreq/qcom,mincpubw*/governor cpufreq
+
+# scheduler
+write /proc/sys/kernel/sched_small_task 30
+
+# re-enable thermal and BCL hotplug
+write /sys/module/msm_thermal/core_control/enabled 1
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_mask $bcl_hotplug_mask
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask $bcl_hotplug_soc_mask
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
+
+# iosched
+write /sys/block/mmcblk0/queue/scheduler cfq
+write /sys/block/mmcblk0/queue/rotational 0
+write /sys/block/mmcblk0/queue/iostats 0
+
+# virtual memory
+write /proc/sys/vm/swappiness 100
+write /proc/sys/vm/dirty_background_ratio 4
+write /proc/sys/vm/dirty_ratio 10
+write /proc/sys/vm/dirty_writeback_centisecs 3000
+
+# lmk
+write /sys/module/lowmemorykiller/parameters/minfree "18432,23040,27648,32256,36864,46080"
+
+# backlight dimmer
+write /sys/module/mdss_fb/parameters/backlight_dimmer 1
