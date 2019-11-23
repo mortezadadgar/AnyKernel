74f758e97b2c33582352e905b5e6753763b28c34
mortezadadgar <mortezadadgar97@gmail.com>
2019-11-23 12:43:49 +0330

    Ramdisk: Tune ext4 options
    * Increase commit interval
    * Set data as writeback mode
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -8,6 +8,7 @@ kernel.string=Shadow Kernel for the Nexus 5x by @mr.ace
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
+
 do.cleanuponabort=0
 device.name1=bullhead
 device.name2=
@@ -62,6 +63,7 @@ dump_boot;
 # fstab.bullhead
 insert_line fstab.bullhead "data           f2fs" after "data           ext4" "/dev/block/platform/soc.0/f9824900.sdhci/by-name/userdata     /data           f2fs    nosuid,nodev,noatime,discard,fsync_mode=nobarrier,background_gc=off wait,check,quota,formattable,encryptable=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata";
 patch_fstab fstab.bullhead none swap flags "zramsize=533413200" "zramsize=1066826400";
+patch_fstab fstab.bullhead /data ext4 options "noatime,nosuid,nodev,barrier=1,data=ordered,nomblk_io_submit,noauto_da_alloc,errors=panic,inode_readahead_blks=8" "noatime,nosuid,nodev,barrier=1,data=writeback,nomblk_io_submit,noauto_da_alloc,errors=panic,inode_readahead_blks=8,commit=300";
 
 # init.exec.rc
 insert_line init.bullhead.rc "init.exec.rc" after "import init.bullhead.ramdump.rc" "import init.exec.rc";
