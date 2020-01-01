154a0602496f04bf66e3f4be60d3e1153c9cc0ba
flipflop133 <48946818+flipflop133@users.noreply.github.com>
2020-01-01 08:40:20 +0100

    anykernel.sh: adapt for Tab A

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=Shadow Kernel for the Samsung Tab A 10.1 by @mr.ace
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=gta3xlwifi
+device.name2=
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/13500000.dwmmc0/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -34,30 +34,31 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+# Shadow Banner
+ui_print " ____  _   _    _    ____   _____        __"
+ui_print "/ ___|| | | |  / \  |  _ \ / _ \ \      / /"
+ui_print "\___ \| |_| | / _ \ | | | | | | \ \ /\ / / "
+ui_print " ___) |  _  |/ ___ \| |_| | |_| |\ V  V /  "
+ui_print "|____/|_| |_/_/   \_\____/ \___/  \_/\_/   "
+ui_print "                                           "
+ui_print " ____                                      "
+ui_print "| __ ) _   _                               "
+ui_print "|  _ \| | | |                              "
+ui_print "| |_) | |_| |                              "
+ui_print "|____/ \__, |                              "
+ui_print "       |___/                               "
+ui_print " __  __           _    ____ _____          "
+ui_print "|  \/  |_ __     / \  / ___| ____|         "
+ui_print "| |\/| | '__|   / _ \| |   |  _|           "
+ui_print "| |  | | |     / ___ \ |___| |___          "
+ui_print "|_|  |_|_|    /_/   \_\____|_____|         "
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
 # end ramdisk changes
 
 write_boot;
 ## end install
-
